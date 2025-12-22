3b0b0d432be26c4994d50d1f012783543d62cbd6
Federico Sanchini <federicosanchini@Mac.station>
2025-12-22 14:51:33 +0100

    test gitignore

--- trading/tester.py
+++ trading/tester.py
@@ -6,6 +6,8 @@ from hyperliquid.exchange import Exchange
 from hyperliquid.info import Info
 from hyperliquid.utils import constants
 
+#TEST
+
 # =====================
 # CONFIG
 # =====================
